34e5e218a78687a716b15fd15460cefcb8ab161f
Shivan Sivakumaran <me@shivan.xyz>
2026-01-20 00:03:01 +1300

    chore: add prerender

--- src/routes/feed.xml/+server.ts
+++ src/routes/feed.xml/+server.ts
@@ -1,5 +1,7 @@
 import { feed } from '$lib/feed';
 
+export const prerender = true;
+
 export async function GET() {
 	return new Response(feed.rss2(), {
 		headers: { 'Content-Type': 'application/xml; charset=utf-8' }

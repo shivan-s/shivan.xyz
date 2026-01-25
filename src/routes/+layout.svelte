be5bab52f101204543f5fa72e3f95ada8a00d848
Shivan Sivakumaran <51132467+shivan-s@users.noreply.github.com>
2026-01-26 09:55:32 +1300

    chore: fix meta tags (#124)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,6 @@
 	import { config } from '$lib/config';
 	import { Footer, Header } from '$lib/components/ui';
 	import { page } from '$app/state';
-	import Shivan from '$lib/assets/img/shivan.png';
 
 	let { children } = $props();
 
@@ -13,9 +12,10 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta property="og:title" content={title} />
+	<meta property="og:title" content={config.description} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content={page.url.toString()} />
-	<meta property="og:image" content={Shivan} />
+	<meta property="og:image" content={config.image} />
 </svelte:head>
 
 <Header />

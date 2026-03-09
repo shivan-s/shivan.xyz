f4b05a25c24a467ffd103439afac27d341b8cc7e
Shivan Sivakumaran <me@shivan.xyz>
2026-03-09 18:49:40 +1300

    chore: add comment

--- src/routes/presentations/[slug]/+page.ts
+++ src/routes/presentations/[slug]/+page.ts
@@ -3,6 +3,7 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { config } from '$lib/config';
 
+/** Required for prerendering presentation without an index file */
 export const entries = () => {
 	const presentations = fetchAllPresentations();
 	return presentations.map(({ slug }) => ({ slug }));

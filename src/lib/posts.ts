946b44c0b686de27fb2eaf9fb34cdccfe88175aa
Shivan Sivakumaran <me@shivan.xyz>
2026-09-10 18:08:43 +1000

    chore: improve the images for headers

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -3,7 +3,10 @@ import { PostSchema } from './schemas';
 import { error } from '@sveltejs/kit';
 
 // NOTE: needs to be a string and start with `../` See: {@link https://vite.dev/guide/features#glob-import}
-const paths = import.meta.glob('../../content/posts/**/index.md', { eager: true });
+const paths = import.meta.glob('../../content/posts/**/index.md', {
+	eager: true,
+	query: { enhanced: true }
+});
 
 /**
  * Fetch all posts
